25b686c84a521d2047888d21b9c06f542b93246d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2019-12-14 11:22:57 +0100

    Add files via upload

--- application/athlete.py
+++ application/athlete.py
@@ -8,20 +8,33 @@ class Athlete:
     self.sport= sport
   def personalisation(self):
     print("le nom est %s et l'age est de: %s"%(self.nom,self.age))
-  def categorie(self,age):
-    if age<15:
+  def categorie(self):
+    if self.age<15:
         print('l athlete  est mineur')
-    elif age<18:
+    elif self.age<18:
         print('l athlete est cadet')
     else:
         print('senior')
         
 
     
-
-p1 = Athlete("khalil", 29,'kick boxing')
-p2 = Athlete("Adel", 39,'basket-ball')
+def controle(age):
+    while(age<5):
+        print('donner un age >5')
+        age=int(input())
+    return age
+#p1 = Athlete("khalil", 9,'kick boxing')
+#p2 = Athlete("Adel", 39,'basket-ball')
+print('donner le nom')
+nom=input()
+print('donner le sport exercé')
+sport=input()
+age=-1
+age=controle(age)
+p1 = Athlete(nom, age,sport)
 
 print(p1.nom)
 print(p1.age)
-print(p2.categorie(p2.age)) 
+print(p1.categorie()) 
+
+    
